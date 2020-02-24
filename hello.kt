d34ae3d1b8a86accb62f193589060f02fe2defae
Качановский Алексей Владимирович <51433@metz.local>
2020-02-24 13:49:23 +0300

    1 commit

---
+++ hello.kt
@@ -0,0 +1,14 @@
+fun main() 
+{
+    fun func() 
+    {
+        val i: Int = 5
+        if (i > 5) {
+            println("nothing")
+        } else {
+            println("Hello, Kt!")
+        }
+    }
+
+    func()
+}
